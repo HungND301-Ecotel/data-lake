b81d70e6bbf51bfdbebeac883ac6e4bca66c5a8f
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2026-02-12 13:42:03 +0700

    Update vite.config.ts

--- fe-modul-report/vite.config.ts
+++ fe-modul-report/vite.config.ts
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/datalake-api": {
+        target: "http://42.113.54.206:1313",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/datalake-api/, ""),
+      },
+    },
+  },
 })
