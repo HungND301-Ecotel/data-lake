b4b492fd25ca35470002a022f03110cd77049d83
unknown <minh01645720374@gmail.com>
2026-03-02 21:14:53 +0700

    update change password fe

--- fe-modul-report/src/features/auth/api/userApi.ts
+++ fe-modul-report/src/features/auth/api/userApi.ts
@@ -1,35 +1,40 @@
 import axiosClient from "../../../services/axiosClient";
-import type { LoginResponse, UserLogin, UserRequest, UserResponse } from "../../employee/types/user";
+import type { LoginResponse, UserLogin, UserRequest, UserResponse, ChangePasswordRequest } from "../../employee/types/user";
 
 export const userApi = {
-    addUser: async (request: UserRequest): Promise<string> => {
-      const res = await axiosClient.post("/user", request);
-      return res.data;
-    },
+  addUser: async (request: UserRequest): Promise<string> => {
+    const res = await axiosClient.post("/user", request);
+    return res.data;
+  },
 
-    updateUser: async (request: UserRequest): Promise<string> => {
-        const res = await axiosClient.put("/user", request);
-        return res.data;
-    },
+  updateUser: async (request: UserRequest): Promise<string> => {
+    const res = await axiosClient.put("/user", request);
+    return res.data;
+  },
 
   login: async (request: UserLogin): Promise<LoginResponse> => {
     const res = await axiosClient.post("/user/login", request);
     return res.data;
   },
-  
-    deleteEmployee: async (employeeId: String | null): Promise<string> => {
-      const res = await axiosClient.delete(`/user/${employeeId}`);
-      return res.data;
-    },
 
-    getByEmployeeId: async (employeeId: String | null): Promise<UserResponse> => {
-      const res = await axiosClient.get(`/user/employee/${employeeId}`);
-      return res.data;
-    },
+  deleteEmployee: async (employeeId: String | null): Promise<string> => {
+    const res = await axiosClient.delete(`/user/${employeeId}`);
+    return res.data;
+  },
 
-    getMyAccount: async (): Promise<UserResponse> => {
-      const res = await axiosClient.get(`/user/my-account`);
-      return res.data;
-    },
-    
+  getByEmployeeId: async (employeeId: String | null): Promise<UserResponse> => {
+    const res = await axiosClient.get(`/user/employee/${employeeId}`);
+    return res.data;
+  },
+
+  getMyAccount: async (): Promise<UserResponse> => {
+    const res = await axiosClient.get(`/user/my-account`);
+    return res.data;
+  },
+
+  changePassword: async (
+    request: ChangePasswordRequest
+  ): Promise<void> => {
+    await axiosClient.put("/user/change-password", request);
+  },
 };
