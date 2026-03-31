d547e4802e85253ca291de8abdd074ada70e268a
unknown <minh01645720374@gmail.com>
2026-03-31 11:36:19 +0700

    update link dev push

--- be/src/main/java/com/quangnt0000/be_modul/config/WebClientConfig.java
+++ be/src/main/java/com/quangnt0000/be_modul/config/WebClientConfig.java
@@ -24,7 +24,7 @@ public class WebClientConfig {
     @Bean
     @Qualifier("vinacominWebClient")
     public WebClient webClient() {
-        String baseUrl = "https://apidatabi.vinacomin.vn";
+        String baseUrl = "https://dev-apidatabi.vinacomin.vn";
         int connectionTimeout = 5000;
         int readTimeout = 10000;
 
