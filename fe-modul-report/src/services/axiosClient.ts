2ae7f5b33a333f4fa84efe787260472fdac836ce
Tài corn <taicorn2004@gmail.com>
2026-06-08 11:53:34 +0700

    feat: implement authentication page and configure secure axios client with interceptors (#74)

--- fe-modul-report/src/services/axiosClient.ts
+++ fe-modul-report/src/services/axiosClient.ts
@@ -11,7 +11,7 @@ const axiosClient = axios.create({
 
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
+  if (token && !config.url?.includes("/user/login")) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
