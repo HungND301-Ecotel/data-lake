244d0dc0f339e0209199ca9ed8c3ef29b5a3e760
pa1tJj <phtuan14@gmail.com>
2026-06-22 12:03:53 +0700

    fix: missing data for name column

--- be/src/main/java/com/quangnt0000/be_modul/repository/DataWH/WareBatchJdbc.java
+++ be/src/main/java/com/quangnt0000/be_modul/repository/DataWH/WareBatchJdbc.java
@@ -21,6 +21,7 @@ public class WareBatchJdbc {
                     SELECT 
                         wb.id AS id,
                         wb.code AS code,
+                        wb.name AS name,
                         wt.table_code AS tableCode,
                         wt.table_name AS reportName,
                         wb.description AS description,
