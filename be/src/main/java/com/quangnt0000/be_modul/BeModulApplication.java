a3a6f50eed137038caafc9edd5339706c918b4e1
Phạm Quốc Trung <trungpqvn1203@gmail.com>
2026-06-22 11:43:18 +0700

    Update

--- be/src/main/java/com/quangnt0000/be_modul/BeModulApplication.java
+++ be/src/main/java/com/quangnt0000/be_modul/BeModulApplication.java
@@ -3,18 +3,12 @@ package com.quangnt0000.be_modul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import io.github.cdimascio.dotenv.Dotenv;
-
 
 @SpringBootApplication
 @EnableFeignClients
 public class BeModulApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(e ->
-				System.setProperty(e.getKey(), e.getValue())
-		);
 		SpringApplication.run(BeModulApplication.class, args);
 	}
 }
